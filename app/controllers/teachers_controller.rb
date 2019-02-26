ac19d0ce968913a63103ee127b3dd9ee577fa5aa
Albrecht Bernstorff <albrecht.bernstorff@web.de>
2019-02-26 12:20:55 +0200

    last fix to the index method of teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,16 +2,15 @@ class TeachersController < ApplicationController
   def index
     if params[:query].present?
       @teachers = Teacher.find_by_skill_or_subject(params[:query])
-      @map_teachers = Teacher.where.not(latitude: nil, longitude: nil)
-      @markers = @map_teachers.map do |teacher|
-        {
-          lng: teacher.longitude,
-          lat: teacher.latitude,
-          infoWindow: "#{teacher.first_name} #{teacher.first_name}"
-        }
-      end
     else
       @teachers = Teacher.all
+    end
+    @markers = @teachers.map do |teacher|
+      {
+        lng: teacher.longitude,
+        lat: teacher.latitude,
+        infoWindow: "#{teacher.first_name} #{teacher.first_name}"
+      }
     end
   end
 
