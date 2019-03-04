8862806097b395871fc7a5a72cefc6abe6ba6e50
liana griffin <lianaelise@hotmail.com>
2019-03-04 17:02:33 +0200

    chats reformated

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,6 +3,7 @@ class Teacher < User
   has_many :skills
   has_many :subjects, through: :skills
   has_many :appointments
+
   mount_uploader :picture, PhotoUploader
 
   validates :subjects, length: {maximum: 3}
